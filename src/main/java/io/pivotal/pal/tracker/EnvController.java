74e74cc04f37caad6bb777429573c9b3f3b5881b
ejaz <mohdejaz74@gmail.com>
2019-12-02 22:45:01 -0600

    Configuring an App

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,32 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController
+{
+  Map<String, String> env = new HashMap<>();
+
+  public EnvController(
+      @Value("${PORT:NOT SET}") String port,
+      @Value("${MEMORY_LIMIT:NOT SET}") String memory_limit,
+      @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+      @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr) {
+
+    env.put("PORT", port);
+    env.put("MEMORY_LIMIT", memory_limit);
+    env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+    env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+
+  }
+
+  @GetMapping("/env")
+  public Map<String,String> getEnv() {
+    return env;
+  }
+}
